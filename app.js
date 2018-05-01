a10083ead08aa44f005c14f51541a8c475523cd3
Shawn <shawn@practera.com>
2018-05-01 17:47:32 +1000

    test

--- app.js
+++ app.js
@@ -19,21 +19,21 @@ app.post('/', function(req, res) {
 	var params = {
 	  	Bucket: "css.practera.com"
  	};
- 	s3.listObjects(params, function(err, data) {
-	   	if (err) {
-			return console.log(err, err.stack); 
-	   	}
-	   	console.log(data);
-	});
+ // 	s3.listObjects(params, function(err, data) {
+	//    	if (err) {
+	// 		return console.log(err, err.stack); 
+	//    	}
+	//    	console.log(data);
+	// });
   // gulp.src(['./source/scss/practera.scss'])
   //   .pipe(sass())
   //   .pipe(cleanCss({
   //     keepSpecialComments: 0
   //   }))
   //   .pipe(gulp.dest('./www/css/'))
-  // res.status(200).json({
-  //   status: 'success'
-  // });
+  res.status(200).json({
+    status: 'success'
+  });
 });
 
 if (app.get('env') === 'local') {
